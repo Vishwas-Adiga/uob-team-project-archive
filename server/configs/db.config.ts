1acb20cef2b0eb6f07819ee84f15fb682df75528
Vishwas Adiga <vxa205@student.bham.ac.uk>
2023-02-24 17:29:08 +0000

    Integrate postgres with Express
    * fix: Mark pg-native as esbuild external
    
    * feat: Add health check api endpoint
    
    * feat: Add pg dependency

---
+++ server/configs/db.config.ts
@@ -0,0 +1,7 @@
+export const DbConfig = {
+  user: "teamproject",
+  password: "",
+  database: "teamproject",
+  host: "localhost",
+  port: 5432,
+};
--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -1,1 +1,13 @@
-// User db-level access goes here
+import pg from "pg";
+import { DbConfig } from "../configs/db.config.js";
+const { Pool, Client } = pg;
+
+const pool = new Pool(DbConfig);
+
+export const getHealth = () => {
+  return new Promise((resolve, reject) => {
+    pool.query("SELECT NOW()", (err, res) => {
+      resolve([err, res]);
+    });
+  });
+};
