000342ffe2d31024573a546b1a16c7740e38a08b
Mohammad Hassan <mah160@student.bham.ac.uk>
2023-03-17 17:12:27 +0000

    feat: Add location widget
    * fix: suppress ts error for attribution
    
    * feat: create vertical slice features for location widget

--- server/controllers/widgets.controller.ts
+++ server/controllers/widgets.controller.ts
@@ -1,6 +1,8 @@
 import { Response } from "express";
 import { RichText } from "../models/richtext.model.js";
 import { AuthenticatedRequest } from "../middleware/jwt.middleware.js";
+import { Location } from "../models/location.model.js";
+
 export const getBioWidget = async (
   req: AuthenticatedRequest,
   res: Response
@@ -13,4 +15,19 @@ export const getBioWidget = async (
   return res.status(200).send({
     richText,
   });
+};
+
+export const getLocationWidgets = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  const location = await Location.findAll({
+    where: {
+      user: req.body.userId,
+    },
+  });
+  if (!location) {
+    return res.status(404).send();
+  }
+  return res.status(200).send(location);
 };
--- server/routes/widgets.route.ts
+++ server/routes/widgets.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getBioWidget } from "../controllers/widgets.controller.js";
+import { getLocationWidgets } from "../controllers/widgets.controller.js";
 
 //TODO Verify token for widgets when database is done:
 
 export const widgetsRouter = Router();
 
 widgetsRouter.get("/rich-text", getBioWidget);
+widgetsRouter.get("/location", getLocationWidgets);
