61f417cf156beaf3707b0e6fd915e55ad6c7e499
Zehra Virjee <zsv147@student.bham.ac.uk>
2023-03-17 16:47:12 +0000

    feat: Add rich-text widget
    * feat: create vertical slices for rich-text

--- server/controllers/widgets.controller.ts
+++ server/controllers/widgets.controller.ts
@@ -1,5 +1,16 @@
-import {Request, Response} from "express";
-
-export const getWidget = async (req: Request, res: Response) => {
-    return res.send({});
+import { Response } from "express";
+import { RichText } from "../models/richtext.model.js";
+import { AuthenticatedRequest } from "../middleware/jwt.middleware.js";
+export const getBioWidget = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  const richText = await RichText.findAll({
+    where: {
+      user: req.body.userId,
+    },
+  });
+  return res.status(200).send({
+    richText,
+  });
 };
