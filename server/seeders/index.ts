f83968881399fdbe35ccd5b8c45149d12b9dd2a2
Vishwas Adiga <vxa205@student.bham.ac.uk>
2023-04-24 13:09:28 +0100

    fix: Drop database before seeding

--- server/seeders/index.ts
+++ server/seeders/index.ts
@@ -1,15 +1,10 @@
-import { Accommodation, Course, Module, User } from "../models/index.js";
+import { Accommodation, Course, Module, sequelize } from "../models/index.js";
 import ACCOMMODATION_DATA from "./accommodation-data.json" assert { type: "json" };
 import COURSE_DATA from "./course-data.json" assert { type: "json" };
 import MODULE_DATA from "./module-data.json" assert { type: "json" };
 
 export const seed = async () => {
-  await User.destroy({ truncate: true });
-  await Promise.all([
-    Accommodation.destroy({ truncate: true }),
-    Course.destroy({ truncate: true }),
-    Module.destroy({ truncate: true }),
-  ]);
+  await sequelize.drop();
   await Promise.all([
     Accommodation.bulkCreate(ACCOMMODATION_DATA),
     Course.bulkCreate(COURSE_DATA),
