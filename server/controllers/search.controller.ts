0c26e63a3a53e198d6846955f9a436855b69eaf7
Vishwas Adiga <vxa205@student.bham.ac.uk>
2023-04-30 14:11:00 +0000

    fix: Make search case-insensitive
    * fix: Make search case-insensitive

--- server/controllers/search.controller.ts
+++ server/controllers/search.controller.ts
@@ -1,16 +1,14 @@
 import { Response } from "express";
 import { ValidatedRequest } from "../middleware/jwt.middleware.js";
-import { User } from "../models/index.js";
+import { sequelize, User } from "../models/index.js";
 import { Op } from "sequelize";
 
 export const getSearchResult = async (req: ValidatedRequest, res: Response) => {
   const query = req.query.query as string;
   const result = await User.findAll({
-    where: {
-      name: {
-        [Op.substring]: query,
-      },
-    },
+    where: sequelize.where(sequelize.fn("LOWER", sequelize.col("name")), {
+      [Op.substring]: query.toLowerCase(),
+    }),
     attributes: {
       include: ["name", "profilePicture", "userId"],
     },
