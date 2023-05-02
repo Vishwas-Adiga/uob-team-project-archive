7b47b53178e35a89030f3c66b82e576d28982202
Vishwas Adiga <vxa205@student.bham.ac.uk>
2023-05-02 12:25:48 +0000

    refactor: Make preferences page responsive
    * fix: Re-order panels
    
    * refactor: Make preferences page responsive

--- client/routes/graph/index.tsx
+++ client/routes/graph/index.tsx
@@ -3,6 +3,7 @@ import { Config } from "../../config";
 import styles from "./style.module.scss";
 import { StudentGraph } from "../../components/graph";
 import { useData } from "../../utils/use-data";
+import { Helmet } from "react-helmet";
 
 export const Graph = () => {
   const [graph] = useData<any | null>(`graph`);
@@ -10,6 +11,11 @@ export const Graph = () => {
   return (
     <AuthenticatedRoute>
       <div className={styles.container}>
+        <Helmet>
+          <title>
+            {Config.GRAPH.NAME} | {Config.APP.NAME}
+          </title>
+        </Helmet>
         <h3>{Config.GRAPH.NAME}</h3>
         <h4>Explore and expand the {Config.GRAPH.NAME}</h4>
         <StudentGraph graph={graph ?? { nodes: [], links: [] }} />
--- client/routes/preferences/index.tsx
+++ client/routes/preferences/index.tsx
@@ -19,11 +19,12 @@ import { Accommodation, Course } from "../../state/types";
 import { Routes } from "../index";
 import { useNavigate } from "react-router-dom";
 import { scannerState } from "../../state/scanner-state";
+import { Helmet } from "react-helmet";
 
 enum accountTypes {
   "Public",
-  "Private",
   "Local",
+  "Private",
 }
 
 const NFC_SUPPORTED = "NDEFReader" in window;
@@ -83,6 +84,8 @@ export const Preferences = () => {
       });
       if (!response.ok) {
         alert("Failed to update profile. Please try again.");
+      } else {
+        window.alert("Updated profile");
       }
     };
     const updateProfile = async () => {
@@ -112,127 +115,141 @@ export const Preferences = () => {
 
   return (
     <AuthenticatedRoute>
-      <>
-        <h1>Preferences</h1>
-        <div className={styles.container}>
-          <div className={styles.picture}>
-            <label>
-              Profile Picture:
-              <img
-                src={
-                  profilePicture
-                    ? URL.createObjectURL(profilePicture)
-                    : `/api/v1/portfolios/${user?.userId}/profile-picture`
-                }
-                alt="Profile Picture"
-              />
-              <input
-                type="file"
-                accept="image/*"
-                onChange={handlePictureChange}
-              />
-            </label>
-          </div>
-
-          <div className={styles.accountTypeSwitcher}>
-            <ContentSwitcher
-              selectedIndex={Object.values(accountTypes).indexOf(
-                privacy ?? "Public"
-              )}
-              onChange={accountText => {
-                handlePrivacy(accountText.text);
-              }}
-            >
-              <Switch name="account-type" text="Public" />
-              <Switch name="account-type" text="Private" />
-              <Switch name="account-type" text="Local" />
-            </ContentSwitcher>
-          </div>
-
-          <div className={styles.fields}>
-            <Form onSubmit={handleSubmit}>
-              <FormGroup legendText="">
-                <TextInput
-                  id="name"
-                  labelText="Name"
-                  value={name}
-                  onChange={e => setName(e.target.value)}
-                  required
-                />
-              </FormGroup>
-              <FormGroup legendText="">
-                <TextInput
-                  id="email"
-                  labelText="Email"
-                  type="email"
-                  value={email}
-                  pattern={Config.ORG.EMAIL_REGEX.source}
-                  onChange={e => setEmail(e.target.value)}
-                  required
-                />
-              </FormGroup>
-              <FormGroup legendText="">
-                <ComboBox
-                  id="course"
-                  titleText="Course"
-                  items={courses}
-                  itemToString={item => (item ? item.name : "")}
-                  onChange={e => setCourse(e.selectedItem)}
-                  selectedItem={course}
-                  shouldFilterItem={({ item, inputValue }) =>
-                    item.name
-                      .toLowerCase()
-                      .replaceAll(/[^a-zA-Z\d]/gi, "")
-                      .includes(
-                        inputValue.toLowerCase().replaceAll(/[^a-zA-Z\d]/gi, "")
-                      )
-                  }
-                />
-              </FormGroup>
-              <FormGroup legendText="">
-                <ComboBox
-                  id="accommodation"
-                  titleText="Accommodation"
-                  items={accommodations}
-                  itemToString={item => (item ? item.name : "")}
-                  onChange={e => setAccommodation(e.selectedItem)}
-                  selectedItem={accommodation}
-                />
-              </FormGroup>
-              <Button type="submit">Save Changes</Button>
-            </Form>
-            {NFC_SUPPORTED && (
-              <Button
-                onClick={setScannerState.bind(null, {
-                  open: true,
-                  mode: "register",
-                })}
-              >
-                Link student ID
-              </Button>
-            )}
-          </div>
-
-          <div className={styles.banner}>
-            <label>
-              Banner:
-              <img
-                src={
-                  profileBanner
-                    ? URL.createObjectURL(profileBanner)
-                    : `/api/v1/portfolios/${user?.userId}/profile-banner`
-                }
-                alt="Profile Banner"
-              />
-              <input
-                type="file"
-                accept="image/*"
-                onChange={handleBannerChange}
-              />
-            </label>
-          </div>
+      <div className={styles.container}>
+        <Helmet>
+          <title>Profile preferences | {Config.APP.NAME}</title>
+        </Helmet>
+        <div className={styles.header}>
+          <h3>Preferences</h3>
+          <h4>Edit your personal details and account preferences</h4>
         </div>
-      </>
+
+        <div className={`${styles.column} ${styles.fields}`}>
+          <h5>Personal info</h5>
+          <Form onSubmit={handleSubmit}>
+            <FormGroup legendText="">
+              <TextInput
+                id="name"
+                labelText="Name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                required
+              />
+            </FormGroup>
+            <FormGroup legendText="">
+              <TextInput
+                id="email"
+                labelText="Email"
+                type="email"
+                value={email}
+                pattern={Config.ORG.EMAIL_REGEX.source}
+                onChange={e => setEmail(e.target.value)}
+                required
+              />
+            </FormGroup>
+            <FormGroup legendText="">
+              <ComboBox
+                id="course"
+                titleText="Course"
+                items={courses}
+                itemToString={item => (item ? item.name : "")}
+                onChange={e => setCourse(e.selectedItem)}
+                selectedItem={course}
+                shouldFilterItem={({ item, inputValue }) =>
+                  item.name
+                    .toLowerCase()
+                    .replaceAll(/[^a-zA-Z\d]/gi, "")
+                    .includes(
+                      inputValue.toLowerCase().replaceAll(/[^a-zA-Z\d]/gi, "")
+                    )
+                }
+              />
+            </FormGroup>
+            <FormGroup legendText="">
+              <ComboBox
+                id="accommodation"
+                titleText="Accommodation"
+                items={accommodations}
+                itemToString={item => (item ? item.name : "")}
+                onChange={e => setAccommodation(e.selectedItem)}
+                selectedItem={accommodation}
+              />
+            </FormGroup>
+            <Button type="submit">Save Changes</Button>
+          </Form>
+          {NFC_SUPPORTED && (
+            <Button
+              onClick={setScannerState.bind(null, {
+                open: true,
+                mode: "register",
+              })}
+            >
+              Link student ID
+            </Button>
+          )}
+        </div>
+
+        <div className={`${styles.column} ${styles.accountTypeSwitcher}`}>
+          <h5>Profile visibility</h5>
+          <ContentSwitcher
+            selectedIndex={Object.values(accountTypes).indexOf(
+              privacy ?? "Public"
+            )}
+            onChange={accountText => {
+              handlePrivacy(accountText.text);
+            }}
+          >
+            <Switch name="account-type" text="Public" />
+            <Switch name="account-type" text="Local" />
+            <Switch name="account-type" text="Private" />
+          </ContentSwitcher>
+          {privacy === "Public" && (
+            <p>
+              Public profiles are visible to everyone on the internet, including
+              users not signed into {Config.APP.NAME}
+            </p>
+          )}
+          {privacy === "Local" && (
+            <p>
+              Local profiles are only visible to {Config.APP.NAME} users from{" "}
+              {Config.ORG.NAME}
+            </p>
+          )}
+          {privacy === "Private" && (
+            <p>Private profiles are only visible to your connections</p>
+          )}
+        </div>
+
+        <div className={`${styles.column} ${styles.picture}`}>
+          <h5>Profile picture and banner</h5>
+          <label className={styles.profilePicture}>
+            <img
+              src={
+                profilePicture
+                  ? URL.createObjectURL(profilePicture)
+                  : `/api/v1/portfolios/${user?.userId}/profile-picture`
+              }
+              alt="Profile Picture"
+            />
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handlePictureChange}
+            />
+          </label>
+          <label className={styles.banner}>
+            <img
+              src={
+                profileBanner
+                  ? URL.createObjectURL(profileBanner)
+                  : `/api/v1/portfolios/${user?.userId}/profile-banner`
+              }
+              alt="Profile Banner"
+            />
+            <input type="file" accept="image/*" onChange={handleBannerChange} />
+          </label>
+        </div>
+      </div>
     </AuthenticatedRoute>
   );
 };
